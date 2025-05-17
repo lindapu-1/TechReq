2df2c4d5f67a63a9f1970875660032f5465887dc
lindapu-1 <97086254+lindapu-1@users.noreply.github.com>
2025-05-17 11:37:32 +0800

    Delete elicitron directory

--- elicitron/backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from openai import OpenAI
-
-os.environ["OPENAI_API_KEY"] = "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"
-
-# model = "gpt-3.5-turbo-0125"
-model = "gpt-4o"
